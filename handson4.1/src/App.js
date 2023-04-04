125447e50fbc47e565f0c89b2b71bc1eed221d1d
Bali Tok <bali.tok@hotmail.com>
2023-04-04 22:28:18 +0800

    Completed HandsON 4,1

--- handson4.1/src/App.js
+++ handson4.1/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class TickleBox extends React.Component {
+  state = {
+    msg: ""
+  }
+
+  tickles = () => {
+    this.setState({
+      msg: "that tickles!"
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      msg: ""
+    })
+  }
+
+  render() {
+    return(
+      <div onMouseEnter={this.tickles} onMouseLeave={this.reset} style={{"border": "2px solid black", "padding": "15px 30px"}}>
+        <p>{this.state.msg}</p>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TickleBox />
   );
 }
 
