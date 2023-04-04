1b96c7984f2052dcb8ddf635f72c4fa34409d813
Bali Tok <bali.tok@hotmail.com>
2023-04-04 13:47:28 +0800

    Completed HandsON 3.1

--- handson3.1/src/App.js
+++ handson3.1/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class AlertBox extends React.Component {
+  state = {
+    msg: ""
+  }
+
+  render() {
+    return(
+      <div style={{"border": "4px solid black"}}>
+        <p>{this.state.msg}</p>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AlertBox />
   );
 }
 
