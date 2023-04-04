bd14a470d4d6c3a20d3f0d35a11ebb7ca8c7fe5a
Bali Tok <bali.tok@hotmail.com>
2023-04-04 22:43:47 +0800

    Completed HandsON 4.2

--- handson4.2/src/App.js
+++ handson4.2/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+class NumberBox extends React.Component {
+  state = {
+    count: 0
+  }
+
+  increase = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrease = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    return(
+      <div style={{border: "2px solid black", height: "200px", width: "400px"}}>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increase}>Increase</button>
+        <button onClick={this.decrease}>Decrease</button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NumberBox />
   );
 }
 
