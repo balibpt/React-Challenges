18c9faa6fd7e1df3a72ac9c8568d985df9c47fd2
Bali Tok <bali.tok@hotmail.com>
2023-04-06 14:01:49 +0800

    Completed HandsON 5.1

--- handson5.1/src/App.js
+++ handson5.1/src/App.js
@@ -1,24 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+class ContactUs extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    enquiries: "",
+    country: "Singapore",
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { firstName, lastName, enquiries, country } = this.state
+    alert(`First Name: ${firstName}, Last Name: ${lastName}, Enquiry: ${enquiries}, Country: ${country}`)
+  }
+
+  handleFirstNameChange = (e) => {
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  handleLastNameChange = (e) => {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  handleEnquiryChange = (e) => {
+    this.setState({
+      enquiries: e.target.value
+    })
+  }
+
+  handleSelectChange = (e) => {
+    this.setState({
+      country: e.target.value
+    })
+  }
+
+  render() {
+    const {firstName, lastName, enquiries, country} = this.state;
+
+    return(
+      <React.Fragment>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>First Name:</label>
+          <input type="text" placeholder='First Name' onChange={this.handleFirstNameChange} />
+          <label>Last Name:</label>
+          <input type="text" placeholder='First Name' onChange={this.handleLastNameChange}/>
+        </div>
+        <div>
+          <label>Enquiries:</label>
+          <label>Support</label>
+          <input type="radio" name="enquiry" value="Support" onChange={this.handleEnquiryChange}/>
+          <label>Sales</label>
+          <input type="radio" name="enquiry" value="Sales" onChange={this.handleEnquiryChange}/>
+          <label>Marketing</label>
+          <input type="radio" name="enquiry" value="Marketing"  onChange={this.handleEnquiryChange}/>
+        </div>
+        <div>
+          <label>Country:</label>
+          <select onChange={this.handleSelectChange}>
+            <option value="Singapore">Singapore</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="Thailand">Thailand</option>
+          </select>
+        </div>
+        <div>
+          <input type="submit" disabled={ (firstName && lastName && enquiries && country) ? false : true}/>
+        </div>
+      </form>
+      </React.Fragment>
+    )
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContactUs />
   );
 }
 
