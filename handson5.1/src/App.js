d07f9c5b1cd56457b5cff0a07a4bf6cfc36dcdfd
Bali Tok <bali.tok@hotmail.com>
2023-04-06 14:31:19 +0800

    Completed HandsON 5.2

--- handson5.1/src/App.js
+++ handson5.1/src/App.js
@@ -8,12 +8,13 @@ class ContactUs extends React.Component {
     lastName: "",
     enquiries: "",
     country: "Singapore",
+    fruits: []
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const { firstName, lastName, enquiries, country } = this.state
-    alert(`First Name: ${firstName}, Last Name: ${lastName}, Enquiry: ${enquiries}, Country: ${country}`)
+    const { firstName, lastName, enquiries, country, fruits } = this.state
+    alert(`First Name: ${firstName}, Last Name: ${lastName}, Enquiry: ${enquiries}, Country: ${country}, Fruits: ${fruits}` )
   }
 
   handleFirstNameChange = (e) => {
@@ -38,6 +39,19 @@ class ContactUs extends React.Component {
     this.setState({
       country: e.target.value
     })
+  }
+
+  handleFruitsChange = (e) => {
+    if(this.state.fruits.includes(e.target.value)) {
+      const indexToDelete = this.state.fruits.indexOf(e.target.value)
+      this.setState({
+        fruits: [...this.state.fruits.splice(0, indexToDelete), ...this.state.fruits.splice(indexToDelete + 1)]
+      })
+    } else {
+      this.setState({
+        fruits: [...this.state.fruits, e.target.value]
+      })
+    }
   }
 
   render() {
@@ -68,6 +82,13 @@ class ContactUs extends React.Component {
             <option value="Malaysia">Malaysia</option>
             <option value="Thailand">Thailand</option>
           </select>
+        </div>
+        <div>
+          <label>Fruits: </label>
+          <input type="checkbox" checked={this.state.fruits.includes('apple')} onChange={this.handleFruitsChange} name="fruits" value="apple"/>Apple
+          <input type="checkbox" checked={this.state.fruits.includes('orange')} onChange={this.handleFruitsChange} name="fruits" value="orange"/>Orange
+          <input type="checkbox" checked={this.state.fruits.includes('pineapple')} onChange={this.handleFruitsChange} name="fruits" value="pineapple"/>Pineapple
+          <input type="checkbox" checked={this.state.fruits.includes('durian')} onChange={this.handleFruitsChange} name="fruits" value="durian"/>Durian
         </div>
         <div>
           <input type="submit" disabled={ (firstName && lastName && enquiries && country) ? false : true}/>
