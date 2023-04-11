981326ffceda0e56c3328c58eac2f698b5559d09
Bali Tok <bali.tok@hotmail.com>
2023-04-11 17:57:52 +0800

    Completed HandsON 12.4

--- handson12.4/src/pages/Splash.js
+++ handson12.4/src/pages/Splash.js
@@ -5,14 +5,22 @@ export default function Splash() {
   const navigate = useNavigate();
 
   function redirect() {
-    navigate("/game");
+    setTimeout(() => {
+      navigate("/game");
+    }, 5000);
   }
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     redirect();
+  //   }, 5000);
+  // });
 
   return (
     <React.Fragment>
       <h1>WELCOME!</h1>
       <p>Redirecting in 5 seconds</p>
-      {setInterval(redirect, 5000)}
+      {redirect()}
     </React.Fragment>
   );
 }
